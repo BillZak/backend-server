53cfabd09e1ba0747d4c0bd31bb72b5c9e51aeb0
Bill Zack <bzak954@gmail.com>
2022-01-18 12:17:40 +0000

    add index.html

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const http = require("http");
+const express = require("express");
 
-const handleAllRequests = (res, req) => {
-  const url = req.url;
+const server = express();
 
-  if (url == "/") res.write("Home Page");
-  else if (url == "/about") res.write("About Page");
-
+const handleProfileReq = (req, res) => {
+  res.send("<h1>Profile page</h1>");
   res.end();
 };
 
-const server = http.createServer(handleAllRequests);
+server.get("/", (req, res) => {
+  res.send("Get request to the homepage");
+  res.end();
+});
+
+server.post("/", (req, res) => {
+  res.send("Post request to the homepage");
+  res.end();
+});
+
+// server.use("/profile/", handleProfileReq);
+
+// server.use("/", (req, res) => {
+//   res.send("Home page");
+//   res.end();
+// });
 
 server.listen(8000, () => console.log("Server is running on port 8000"));
